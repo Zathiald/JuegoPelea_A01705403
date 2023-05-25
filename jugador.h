195a263f435c1b8a0c3f8e9dc25de636704cbabf
Zathiald <111139805+Zathiald@users.noreply.github.com>
2023-05-24 22:03:40 -0600

    Add files via upload

--- jugador.h
+++ jugador.h
@@ -64,22 +64,22 @@ void Jugador::setnombreAtaque2(string nombre_Ataque2) {
 void Jugador::atacarContrincante(float vidaCont, float vidaJug, float fuerzaCont, float fuerzaJug) {
 	int contrincanteAtaque, jugadorChance;
 	contrincanteAtaque = rand() % 2;
-	jugadorChance = rand() % 1;
+	jugadorChance = rand() % 3;
 	if (contrincanteAtaque == 0) {
-		if (jugadorChance == 0) {
+		if (jugadorChance == 3) {
 			cout << "TU ATAQUE HA FALLADO...pero el enemigo tampoco logro hacer nada" << endl;
 		}
-		if (jugadorChance == 1) {
+		if (jugadorChance == 0 || jugadorChance == 1 || jugadorChance == 2) {
 			vidaCont = vidaCont - fuerzaJug;
 			cout << "HAS LOGRADO ATACAR!!!, la vida del enemigo ahora es: " << vidaCont<< endl;
 		}
 	}
 	if (contrincanteAtaque == 1) {
-		if (jugadorChance == 0) {
+		if (jugadorChance == 3) {
 			vidaJug = vidaJug - fuerzaCont;
 			cout << "TU ATAQUE HA FALLADO y peor aun EL ENEMIGO SI TE ATACO, tu vida ahora es: " << vidaJug << endl;
 		}
-		if (jugadorChance == 1) {
+		if (jugadorChance == 0 || jugadorChance == 1 || jugadorChance == 2) {
 			vidaJug = vidaJug - (fuerzaCont / 2);
 			vidaCont = vidaCont - (fuerzaJug / 2);
 			cout << "AMBOS SE HAN ATACADO MUTUAMENTE, tu vida ahora es: " << vidaJug << endl;
@@ -87,10 +87,10 @@ void Jugador::atacarContrincante(float vidaCont, float vidaJug, float fuerzaCont
 		}
 	}
 	if (contrincanteAtaque == 2) {
-		if (jugadorChance == 0) {
+		if (jugadorChance == 3) {
 			cout << "TU ATAQUE HA FALLADO, pero el enemigo se estaba defendiendo así que no paso nada al final "<< endl;
 		}
-		if (jugadorChance == 1) {
+		if (jugadorChance == 0 || jugadorChance == 1 || jugadorChance == 2) {
 			cout << "HAS LOGRADO ATACAR!!, pero el enemigo se defendio, nada cambio" << endl;
 		}
 	}
